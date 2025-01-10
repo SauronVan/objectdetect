ecbccf30c1cc9e7da01e78b24c2ce5e527ac6e75
bluelul <bluelul.email@gmail.com>
2025-01-10 15:41:00 +0700

    ep 2

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,6 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+import 'dart:developer';
 
 class ScanController extends GetxController {
 
@@ -8,6 +10,7 @@ class ScanController extends GetxController {
   void onInit() {
     super.onInit();
     initCamera();
+    initTFLite();
   }
 
   @override
@@ -20,6 +23,7 @@ class ScanController extends GetxController {
   late List<CameraDescription> cameras;
 
   var isCameraInitialized = false.obs;
+  var cameraCount = 0;
 
   initCamera() async {
     if (await Permission.camera.request().isGranted) {
@@ -28,12 +32,51 @@ class ScanController extends GetxController {
       cameraController = CameraController(
         cameras[0],
         ResolutionPreset.max,
+        imageFormatGroup: ImageFormatGroup.bgra8888,
       );
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+          cameraController.startImageStream((image) {
+            cameraCount++;
+            if (cameraCount % 10 == 0) {
+              cameraCount = 0;
+              objectDetector(image);
+            }
+            update();
+          });
+      });
       isCameraInitialized(true);
       update();
     } else {
-      print("Permission denied");
+      log("Permission denied");
+    }
+  }
+
+  initTFLite() async {
+    await Tflite.loadModel(
+      model: "assets/mobilenet_v1_1.0_224.tflite",
+      labels: "assets/mobilenet_v1_1.0_224.txt",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(bytesList: image.planes.map((e){
+      return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+
+    if (detector != null) {
+      log("Result is $detector");
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AI Object Detection',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
